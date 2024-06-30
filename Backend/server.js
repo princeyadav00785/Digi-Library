b4fbdf484c33cc2b2387692d78058eb070c4b731
Prince Yadav <princeyadav00785@gmail.com>
2024-07-01 01:07:21 +0530

    Added User Feedback Route

--- Backend/server.js
+++ Backend/server.js
@@ -8,6 +8,7 @@ const connectDB= require('./DB/db');
 const userRoutes =require('./Routes/userRoutes');
 const bookRoutes =require('./Routes/bookRoutes');
 const adminRoutes=require('./Routes/adminRoutes');
+const feedbackRoutes=require('./Routes/feedbackRoutes')
 
 connectDB();
 
@@ -19,6 +20,7 @@ app.use('/api/auth',userRoutes);
 app.use('/api/books',bookRoutes);
 app.use('/api/book-requests', bookRoutes);
 app.use('/api/admin',adminRoutes);
+app.use('/api/feedback',feedbackRoutes);
 
 
 app.use((err, req, res, next) => {
